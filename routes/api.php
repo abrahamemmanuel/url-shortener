2c6ee7653c2d1c1590b6b1cae4d581a07668da05
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2023-10-30 22:01:02 +0100

    hello world api route test

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//return a basic hello world
+Route::get('/hello', function () {
+    return 'Hello World';
 });
