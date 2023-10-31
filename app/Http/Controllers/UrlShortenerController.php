a1e995c3402b87ef9e96eec5ace212ee3c991106
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2023-10-31 23:31:41 +0100

    refactored controller methods, added service class for url shotener

--- app/Http/Controllers/UrlShortenerController.php
+++ app/Http/Controllers/UrlShortenerController.php
@@ -16,71 +16,21 @@ class UrlShortenerController extends Controller implements UrlShortenerInterface
 {
     public function encode(UrlInputRequest $request): JsonResponse|Response
     {
-        $id = Shortid::generate();
-        $url = $request->input('url');
-        $base_url = $request->root();
-        $short_url = $base_url . '/' . $id;
-        $data = [
-            'long_url' => $url,
-            'short_url' => $short_url,
-            'url_path' => $id,
-            'stats' => [
-                'click_count' => 0,
-                'last_accessed' => null,
-                'date_time_accessed' => [],
-                'referrer' => [],
-                'geographic' => [
-                    'country' => [],
-                    'city' => []
-                ],
-                'device_info' => []
-            ],
-            'created_at' => now(),
-            'updated_at' => now()
-        ];
 
-        Cache::put($id, $data, 60 * 60);
-
-        return response()->json([
-            'message' => $url . ' successfully shortened',
-            'success' => true,
-            'data' => $data['short_url'],
-        ], Response::HTTP_OK);
     }
 
     public function decode(UrlInputRequest $request): JsonResponse|Response
     {
-        $short_url = $request->input('url');
-        $url_path = explode('/', $short_url)[3];
-        $data = Cache::get($url_path);
-        return response()->json([
-            'message' => $short_url . ' successfully decoded',
-            'success' => true,
-            'data' => $data['long_url'],
-        ], Response::HTTP_OK);
+
     }
 
     public function redirect(Request $request, $url_path): JsonResponse|RedirectResponse
     {
-        $data = Cache::get($url_path);
-        $data['stats']['click_count'] += 1;
-        $data['stats']['last_accessed'] = now();
-        $data['stats']['date_time_accessed'][] = now();
-        $data['stats']['referrer'][] = $request->server('HTTP_REFERER');
-        $data['stats']['geographic']['country'][] = $request->server('HTTP_CF_IPCOUNTRY');
-        $data['stats']['geographic']['city'][] = $request->server('HTTP_CF_IPCITY');
-        $data['stats']['device_info'][] = $request->server('HTTP_USER_AGENT') . $_SERVER['REMOTE_ADDR'];
-        Cache::put($url_path, $data, 60 * 60);
-        return redirect($data['long_url']);
+
     }
 
     public function stats(Request $request, $url_path): JsonResponse|Response
     {
-        $data = Cache::get($url_path);
-        return response()->json([
-            'message' => $url_path . ' statistic retrieved successfully',
-            'success' => true,
-            'data' => $data['stats'],
-        ], Response::HTTP_OK);
+
     }
 }
---
+++ app/Services/UrlShortenerService.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\UrlShortenerRepository;
+use PUGX\Shortid\Shortid;
+use Illuminate\Support\Facades\Cache;
+
+class UrlShortenerService
+{
+    protected string $url_path;
+    protected string $long_url;
+    protected string $short_url;
+    protected array $base_url;
+    protected string $id;
+    
+    public function __construct(Request $request = null, string $url_path = null)
+    {
+        $this->id = Shortid::generate();
+        $this->long_url = $request->input('url') ?? null;
+        $this->url_path = explode('/', $request->input('url'))[3] ?? $url_path;
+        $this->base_url = $request->root() ?? null;
+        $this->short_url = $this->base_url . '/' . $this->id ?? null;
+    }
+
+    public function setShortLinkData(): array
+    {
+        return [
+            'long_url' => $this->long_url,
+            'short_url' => $this->short_url,
+            'url_path' => $this->id,
+            'statistic' => [
+                'click_count' => 0,
+                'last_accessed' => null,
+                'date_time_accessed' => [],
+                'referrer' => [],
+                'geographic' => [
+                    'country' => [],
+                    'city' => []
+                ],
+                'device_info' => []
+            ],
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+    }
+
+    public getShortLinkData(): array
+    {
+        try {
+            return Cache::get($this->url_path);
+        } catch (\Throwable $exception) {
+            $this->customErrorMessageHandler($exception);
+        }
+    }
+
+    public function createShortLink(): array
+    {
+        try {
+            return Cache::put($this->id, $this->setShortLinkData(), 60 * 60);
+        } catch (\Throwable $exception) {
+            $this->customErrorMessageHandler($exception);
+        }
+    }
+
+    public function updateShortLink(): array
+    {
+        try {
+            return Cache::put($this->url_path, $this->updateStatisticData(), 60 * 60);
+        } catch (\Throwable $exception) {
+            $this->customErrorMessageHandler($exception);
+        }
+    }
+
+    public function updateStatisticData(): array
+    {
+        $data = $this->getShortLinkData();
+        $data['statistic']['click_count'] += 1;
+        $data['statistic']['last_accessed'] = now();
+        $data['statistic']['date_time_accessed'][] = now();
+        $data['statistic']['referrer'][] = $_SERVER['HTTP_REFERER'];
+        $data['statistic']['geographic']['country'][] = $_SERVER['HTTP_CF_IPCOUNTRY'];
+        $data['statistic']['geographic']['city'][] = $_SERVER['HTTP_CF_IPCITY'];
+        $data['statistic']['device_info'][] = $_SERVER['HTTP_USER_AGENT'];
+        return $data;
+    }
+
+    public function customErrorMessageHandler(object $exception): JsonResponse
+    {
+        return response()->json([
+            'message' => $exception->getMessage(),
+            'success' => false,
+            'data' => null,
+        ], Response::HTTP_NOT_FOUND);
+    }
+}
